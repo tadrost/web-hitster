ba9ba031eac9f0cabfda83ae5ad54821c0b314b3
Teun Drost <159715135+tadrost@users.noreply.github.com>
2026-08-12 23:00:59 +0200

    revert to stable version

--- app/routes.py
+++ app/routes.py
@@ -1,33 +1,25 @@
 from flask import Flask, render_template, url_for, redirect, request
-from app import app
+from app import app, spotify
 from app.forms import EmptyForm
 from random import randint
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
 
-SCOPE = "user-read-currently-playing"
-CLIENT_ID = os.environ["SPOTIFY_CLIENT_ID"]
-CLIENT_SECRET = os.environ["SPOTIFY_CLIENT_SECRET"]
-REDIRECT_URI = os.environ["SPOTIFY_REDIRECT_URI"]
-
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id= CLIENT_ID,
-                                               client_secret= CLIENT_SECRET,
-                                               redirect_uri= REDIRECT_URI,
-                                               scope=SCOPE))
 @app.route('/')
 def index():
-    current_song = sp.currently_playing()
+    current_song = None
     if current_song is not None:
         current_song = current_song["item"]["name"]
 
     form = EmptyForm()
     return render_template('index.html', current_song=current_song, form=form)
 
-# @app.route('/test')
-# def test():
-#     return f"""
-# id: {CLIENT_ID}
-# secret: {CLIENT_SECRET}
-# redirect_uri: {REDIRECT_URI}"""
+@app.route('/test')
+def test():
+    currently_playing = spotify.currently_playing()
+    if currently_playing:
+        song = currently_playing["item"]["name"]
+        album = currently_playing["item"]["album"]["name"]
+    else: song = album = None
+    return render_template('test.html', song=song, album=album) 
